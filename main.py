5f76d0023fcedb2719124e54f76475bfcbf0f9d4
JangJunhyeok21 <jangjh1995@naver.com>
2021-11-15 23:36:12 +0900

    2021_2 오픈소스SW 이해와 실습 중간프로젝트 2021042031 장준혁

--- main.py
+++ main.py
@@ -1,78 +1,98 @@
 import pygame
 import random
 import keyboard
+
 ########################################
 
-#기본 초기화
+# 기본 초기화
 pygame.init()
 
-#화면 크기 설정
-screen_width=700
-screen_height=700
-screen=pygame.display.set_mode((screen_width,screen_height))
+# 화면 크기 설정
+screen_width = 700
+screen_height = 700
+screen = pygame.display.set_mode((screen_width, screen_height))
 
-#FPS
-clock=pygame.time.Clock()
+# FPS
+clock = pygame.time.Clock()
 
-#폰트 설정
+# 폰트 설정
 game_font = pygame.font.Font('source/D2Coding.ttf', 60)
 
 # 창 제목
 pygame.display.set_caption("무단 횡단")
 
 #####################################
-#사용자 게임 초기와 (배경, 이미지, 좌표, 속도 등)
+# 사용자 게임 초기와 (배경, 이미지, 좌표, 속도 등)
 
-total_score=0
-temp_score=0
-total_level=10
-characterSize=(40,70)
-carSize=(25,50)
-is_right=None
-is_left=None
+total_score = 0
+temp_score = 0
+total_level = 10
+characterSize = (40, 70)
+carSize = (25, 50)
+is_right = None
+is_left = None
 
-#배경 이미지 및 효과음 불러오기
-background=pygame.image.load("source/background.png")
-character=[pygame.transform.scale(pygame.image.load("source/character/walk (1).png"),characterSize), pygame.transform.scale(pygame.image.load("source/character/walk (2).png"),characterSize),
-           pygame.transform.scale(pygame.image.load("source/character/walk (3).png"),characterSize), pygame.transform.scale(pygame.image.load("source/character/walk (4).png"),characterSize),
-           pygame.transform.scale(pygame.image.load("source/character/walk (5).png"),characterSize), pygame.transform.scale(pygame.image.load("source/character/walk (6).png"),characterSize),
-           pygame.transform.scale(pygame.image.load("source/character/walk (7).png"),characterSize), pygame.transform.scale(pygame.image.load("source/character/walk (8).png"),characterSize),
-           pygame.transform.scale(pygame.image.load("source/character/walk (9).png"),characterSize), pygame.transform.scale(pygame.image.load("source/character/walk (10).png"),characterSize),
-           pygame.transform.scale(pygame.image.load("source/character/walk (11).png"),characterSize), pygame.transform.scale(pygame.image.load("source/character/walk (12).png"),characterSize),
-           pygame.transform.scale(pygame.image.load("source/character/walk (13).png"),characterSize), pygame.transform.scale(pygame.image.load("source/character/walk (14).png"),characterSize),
-           pygame.transform.scale(pygame.image.load("source/character/walk (15).png"),characterSize) ]
-carsUp=[pygame.transform.scale(pygame.image.load("source/cars/porcheup.png"),carSize), pygame.transform.scale(pygame.image.load("source/cars/benzup.png"),carSize),
-        pygame.transform.scale(pygame.image.load("source/cars/f1up.png"),carSize), pygame.transform.scale(pygame.image.load("source/cars/volvoup.png"),carSize)]
-carsDown=[pygame.transform.scale(pygame.image.load("source/cars/porchedown.png"),carSize), pygame.transform.scale(pygame.image.load("source/cars/benzdown.png"),carSize),
-          pygame.transform.scale(pygame.image.load("source/cars/f1down.png"),carSize), pygame.transform.scale(pygame.image.load("source/cars/volvodown.png"),carSize)]
-clear_sound=pygame.mixer.Sound("source/audio/stage_clear.wav")
-crash_sound=pygame.mixer.Sound("source/audio/traffic_accident.wav")
+running = True  # 게임이 진행중인가 확인하는 변수
+play = False
 
-#캐릭터 정보
-walkCount=1
-character_size=character[walkCount].get_rect().size
-character_width=character_size[0]
-character_height=character_size[1]
-character_x_pos=0
-character_y_pos=screen_height/2 - character_height/2
+# 배경 이미지 및 효과음 불러오기
+background = pygame.image.load("source/background.png")
+readyScreen = pygame.image.load("source/readyScreen/readyscreen.png")
+character = [pygame.transform.scale(pygame.image.load("source/character/walk (1).png"), characterSize),
+             pygame.transform.scale(pygame.image.load("source/character/walk (2).png"), characterSize),
+             pygame.transform.scale(pygame.image.load("source/character/walk (3).png"), characterSize),
+             pygame.transform.scale(pygame.image.load("source/character/walk (4).png"), characterSize),
+             pygame.transform.scale(pygame.image.load("source/character/walk (5).png"), characterSize),
+             pygame.transform.scale(pygame.image.load("source/character/walk (6).png"), characterSize),
+             pygame.transform.scale(pygame.image.load("source/character/walk (7).png"), characterSize),
+             pygame.transform.scale(pygame.image.load("source/character/walk (8).png"), characterSize),
+             pygame.transform.scale(pygame.image.load("source/character/walk (9).png"), characterSize),
+             pygame.transform.scale(pygame.image.load("source/character/walk (10).png"), characterSize),
+             pygame.transform.scale(pygame.image.load("source/character/walk (11).png"), characterSize),
+             pygame.transform.scale(pygame.image.load("source/character/walk (12).png"), characterSize),
+             pygame.transform.scale(pygame.image.load("source/character/walk (13).png"), characterSize),
+             pygame.transform.scale(pygame.image.load("source/character/walk (14).png"), characterSize),
+             pygame.transform.scale(pygame.image.load("source/character/walk (15).png"), characterSize)]
+carsUp = [pygame.transform.scale(pygame.image.load("source/cars/porcheup.png"), carSize),
+          pygame.transform.scale(pygame.image.load("source/cars/benzup.png"), carSize),
+          pygame.transform.scale(pygame.image.load("source/cars/f1up.png"), carSize),
+          pygame.transform.scale(pygame.image.load("source/cars/volvoup.png"), carSize)]
+carsDown = [pygame.transform.scale(pygame.image.load("source/cars/porchedown.png"), carSize),
+            pygame.transform.scale(pygame.image.load("source/cars/benzdown.png"), carSize),
+            pygame.transform.scale(pygame.image.load("source/cars/f1down.png"), carSize),
+            pygame.transform.scale(pygame.image.load("source/cars/volvodown.png"), carSize)]
+clear_sound = pygame.mixer.Sound("source/audio/stage_clear.wav")
+crash_sound = pygame.mixer.Sound("source/audio/traffic_accident.wav")
+start_icon = pygame.transform.scale(pygame.image.load("source/readyScreen/Start.png"), (100, 100))
+quit_icon = pygame.transform.scale(pygame.image.load("source/readyScreen/quit.png"), (100, 100))
 
-#캐릭터 이동 관련
-to_x=0
-to_y=0
+# 캐릭터 정보
+walkCount = 1
+character_size = character[walkCount].get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = 0
+character_y_pos = screen_height / 2 - character_height / 2
+
+# 캐릭터 이동 관련
+to_x = 0
+to_y = 0
 character_speed = 0.5
 
-#장애물 클래스
+# 장애물 클래스
 object_list = list()
-class object_class:
-    object_image=pygame.image.load("source/cars/object.png")
 
-    object_size=object_image.get_rect().size
-    object_width=object_size[0]
-    object_height=object_size[1]
-    object_x_pos=0
-    object_y_pos=0
-    object_speed=0
-    object_rad=0
+
+class object_class:
+    object_image = pygame.image.load("source/cars/object.png")
+
+    object_size = object_image.get_rect().size
+    object_width = object_size[0]
+    object_height = object_size[1]
+    object_x_pos = 0
+    object_y_pos = 0
+    object_speed = 0
+    object_rad = 0
     object_spawnpoint = None
 
     object_rect = object_image.get_rect()
@@ -81,15 +101,15 @@ class object_class:
 
     def __init__(self):
         self.object_speed = random.choice([0.2, 0.4, 0.6]) * dt
-        a=random.randint(1,100)
-        if (a%10==0): #10분의 1의 확률로 차량 소환
+        a = random.randint(1, 100)
+        if (a % 10 == 0):  # 10분의 1의 확률로 차량 소환
             self.object_spawnPoint = random.choice(['UP', 'DOWN'])
         else:
             self.object_spawnPoint = 'NONE'
 
         # 스폰 지점 설정
         if self.object_spawnPoint == 'UP':
-            self.object_image=random.choice(carsDown)
+            self.object_image = random.choice(carsDown)
             self.object_x_pos = random.choice([130, 200, 270, 340, 410, 480, 550])
             self.object_y_pos = - self.object_height
             self.object_rad = 1
@@ -117,24 +137,49 @@ class object_class:
 
         if self.object_spawnPoint == 'NONE':
             object_list.remove(self)
-#충돌 처리를 위한 장애물 위치 확인
+
+    # 충돌 처리를 위한 장애물 위치 확인
     def object_collide(self):
         self.object_rect = self.object_image.get_rect()
         self.object_rect.left = self.object_x_pos
         self.object_rect.top = self.object_y_pos
 
-###############################
 
-#캐릭터 이동 함수
+# 버튼 클래스
+class Button:
+    def __init__(self, img, x, y, width, height, action=None):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        if x + width > mouse[0] > x and y + height > mouse[1] > y:
+            screen.blit(img, (x, y))
+            if click[0]:
+                pygame.time.wait(1)
+                action()
+        else:
+            screen.blit(img, (x, y))
+
+
+###############################
+# 게임 중지 함수
+def quitgame():
+    global running
+    pygame.quit()
+    running=False
+
+def playgame():
+    global play
+    play=True
+
+# 캐릭터 이동 함수
 def character_Move():
-    global walkCount, character_y_pos, character_x_pos, to_x, to_y, is_left,is_right
+    global walkCount, character_y_pos, character_x_pos, to_x, to_y, is_left, is_right
 
     if is_right == True:
         to_x = character_speed
-        walkCount+=1
-    elif is_left ==True:
+        walkCount += 1
+    elif is_left == True:
         to_x = -character_speed
-        walkCount-=1
+        walkCount -= 1
 
     # walkCount가 범위를 넘어가면 초기화
     if walkCount > 13:
@@ -142,27 +187,31 @@ def character_Move():
     elif walkCount < 1:
         walkCount = 14
 
-def score_cal():#우측 도달 시 좌측으로 이동 및 점수 계산, 한 차선 당 1점
-    global character_x_pos
-    if character_x_pos > 620:
+
+# 점수 계산 함수
+def score_cal():  # 우측 도달 시 좌측으로 이동 및 점수 계산, 한 차선 당 1점
+    global character_x_pos, total_score, temp_score
+    if character_x_pos > 610:
         character_x_pos = 0
         clear_sound.play()
         total_score += 7
         temp_score = 0
-    elif character_x_pos > 550:
+    elif character_x_pos >= 550:
         temp_score = 6
-    elif character_x_pos > 480:
+    elif character_x_pos >= 480:
         temp_score = 5
-    elif character_x_pos > 410:
+    elif character_x_pos >= 410:
         temp_score = 4
-    elif character_x_pos > 340:
+    elif character_x_pos >= 340:
         temp_score = 3
-    elif character_x_pos > 270:
+    elif character_x_pos >= 270:
         temp_score = 2
-    elif character_x_pos > 200:
+    elif character_x_pos >= 200:
         temp_score = 1
 
-def boundary():# 범위 제한
+
+# 범위 제한 함수
+def boundary():
     global character_x_pos, character_y_pos
     if character_y_pos < 0:
         character_y_pos = 0
@@ -173,15 +222,23 @@ def boundary():# 범위 제한
     elif character_y_pos > screen_height - character_height:
         character_y_pos = screen_height - character_height
 
+
+# 출력 함수
 def printer():
     # 배경 출력
-    screen.blit(background, (0, 0))
-    # 캐릭터 출력
-    screen.blit(character[walkCount], (character_x_pos, character_y_pos))
-    # 장애물들 출력
-    for i in object_list:
-        i.object_move()
-        screen.blit(i.object_image, (i.object_x_pos, i.object_y_pos))
+    if play:
+        screen.blit(background, (0, 0))
+        # 캐릭터 출력
+        screen.blit(character[walkCount], (character_x_pos, character_y_pos))
+        # 장애물들 출력
+        for i in object_list:
+            i.object_move()
+            screen.blit(i.object_image, (i.object_x_pos, i.object_y_pos))
+    else:
+        screen.blit(readyScreen, (0, 0))
+        screen.blit(start_icon, (80, 455))
+        screen.blit(quit_icon, (480, 455))
+
 
 def crash():
     # 충돌 처리를 위한 캐릭터 위치 확인
@@ -218,57 +275,75 @@ def crash():
                 # esc 혹은 창닫기를 누르면 종료.
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT or keyboard.is_pressed('esc'):
+                        play = False
                         running = False
                         pygame.quit()
 
-#실행문
-running=True #게임이 진행중인가 확인하는 변수
-while running:
-    dt=clock.tick(60)
-    print("fps : "+str(clock.get_fps()))
+# 실행문
 
+while running:
+    dt = clock.tick(60)
+    #print("fps : " + str(clock.get_fps()))
+    printer()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running=False
+            play = False
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            if 180 >= pos[0] >= 80 and 555 >= pos[1] >= 455:
+                playgame()
+            if 570 >= pos[0] >= 480 and 555 >= pos[1] >= 455:
+                quitgame()
+    while play:
+        dt = clock.tick(60)
+        print("fps : " + str(clock.get_fps()))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                play = False
+                running = False
 
-        if event.type==pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                is_right = True
-                is_left = False
-            elif event.key == pygame.K_LEFT:
-                is_right = False
-                is_left = True
-            elif event.key == pygame.K_UP:
-                to_y-=character_speed
-            elif event.key == pygame.K_DOWN:
-                to_y+=character_speed
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    is_right = True
+                    is_left = False
+                elif event.key == pygame.K_LEFT:
+                    is_right = False
+                    is_left = True
+                elif event.key == pygame.K_UP:
+                    to_y -= character_speed
+                elif event.key == pygame.K_DOWN:
+                    to_y += character_speed
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                is_right = False
-                is_left = False
-                to_x = 0
-            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                to_y=0
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    is_right = False
+                    is_left = False
+                    to_x = 0
+                elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    to_y = 0
 
-    #캐릭터 이동
-    character_Move()
-    character_x_pos += to_x * dt
-    character_y_pos += to_y * dt
+        # 캐릭터 이동
+        character_Move()
+        character_x_pos += to_x * dt
+        character_y_pos += to_y * dt
 
-    score_cal() #점수계산
-    boundary() #캐릭터이동범위제한
+        score_cal()  # 점수계산
+        boundary()  # 캐릭터이동범위제한
 
-    #장애물 생성
-    # 두바퀴 마다 장애물 한 씩 추가
-    total_level= (int)(total_score/14 + 10)
-    if total_level >= len(object_list):
-        object_list.append(object_class())
+        # 장애물 생성
+        # 두바퀴 마다 장애물 한 씩 추가
+        total_level = (int)(total_score / 14 + 10)
+        if total_level >= len(object_list):
+            object_list.append(object_class())
 
-    # 배경 및 캐릭터 출력
-    printer()
-    #충돌처리
-    crash()
-    pygame.display.update() #게임화면 리프레쉬
-#pygame 종료
+        # 배경 및 캐릭터 출력
+        printer()
+        # 충돌처리
+        crash()
+        pygame.display.update()  # 게임화면 리프레쉬
+
+    pygame.display.update()  # 게임화면 리프레쉬
+
+# pygame 종료
 pygame.quit()
